243df8ac2aec1bd0d3f126c6423d476c9f6d7f1f
Nguyệt Trang <nguyet@Nguyets-Laptop.local>
2025-11-09 23:53:03 -0800

    add more in setting.py

--- tarot/settings.py
+++ tarot/settings.py
@@ -48,7 +48,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 ROOT_URLCONF = 'tarot.urls'
 
